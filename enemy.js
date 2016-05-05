d3c6a6541af1de1f29686ce734ef8f1d7bff890b
bocky2 <pokedoug@hotmail.com.au>
2016-05-05 21:29:52 +1000

    golem animation continuation
    code fully under way for the golem animations. added right-facing images
    to the spritesheets

--- enemy.js
+++ enemy.js
@@ -1,14 +1,44 @@
 //ENEMY
 
+var golemLEFT = 1;
+var golemRIGHT = 0;
+
+var golemANIM_IDLE_LEFT = 0;
+var golemANIM_IDLE_RIGHT = 1;
+var golemANIM_ATTACK_LEFT = 0;
+var golemANIM_ATTACK_RIGHT = 1;
+var golemANIM_APPEAR_LEFT = 0;
+var golemANIM_APPEAR_RIGHT = 1;
+var golemANIM_DIE_LEFT = 0;
+var golemANIM_DIE_RIGHT = 1;
+
+
 var Enemy = function()
 {
 	this.idleSprite = new Sprite("golem idle-walk.png");
-	this.idleSprite.buildAnimation(6, 1, 228, 174, 0,
+	this.idleSprite.buildAnimation(6, 2, 228, 174, 0,
 		[0, 1, 2, 3, 4, 5,]);
-	for(var i=0; i<ANIM_MAX; i++)
-	{
-		this.idleSprite.setAnimationOffset(i, -55, -87);
-	}
+	this.idleSprite.buildAnimation(6, 2, 228, 174, 0, 
+		[11, 10, 9, 8, 7, 6]);
+	
+	this.attackSprite = new Sprite("golem attack.png");
+	this.attackSprite.buildAnimation(6, 2, 264, 186, 0,
+		[0, 1, 2, 3, 4, 5]);
+	this.attackSprite.buildAnimation(6, 2, 264, 186, 0,
+		[11, 10, 9, 8, 7, 6]);
+
+	this.appearSprite = new Sprite("golem appear.png");
+	this.appearSprite.buildAnimation(15, 2, 228, 174, 0,
+		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+	this.appearSprite.buildAnimation(15, 2, 228, 174, 0,
+		[29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15]);
+		
+	this.dieSprite = new Sprite("golem die.png");
+	this.dieSprite.buildAnimation(7, 2, 228, 252, 0,
+		[0, 1, 2, 3, 4, 5, 6]);
+	this.dieSprite.buildAnimation(7, 2, 228, 252, 0,
+		[13, 12, 11, 10, 9, 8, 7]);
+		
 
 	this.position = new Vector2();
 	this.position.set( 9*TILE, 0*TILE );
@@ -24,12 +54,10 @@ var Enemy = function()
 }
 Enemy.prototype.update = function(deltatime)
 {
-	var posChange = this.velocity.copy();
-	posChange.multiplyScalar(deltatime);
-	this.position.add(posChange);
+	this.sprite.update(deltatime);
 }
 
 Enemy.prototype.draw = function()
 {
-	DrawImage(context, this.image, this.position.x, this.position.y, 0);
+	this.sprite.draw(context, this.position.x, this.position.y);
 }
